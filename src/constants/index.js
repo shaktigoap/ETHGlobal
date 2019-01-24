f6ce39cadf0e6de0c375ab40307f7c5c4e70e4a6
ajayns <ajay.ns@outlook.com>
2019-01-24 08:36:31 +0530

    :sparkles: Shadow, Font constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,14 @@
+const SHADOW = {
+  regular: '0 10px 30px 0 rgba(66,75,158,0.18)',
+  light: '0 2px 4px -5px rgba(1,9,62,0.11)',
+};
+
+const FONT = {
+  primary: `'Now', san-serif`,
+  secondary: `'Muli', sans-serif`,
+};
+
 export { COLOR } from './color';
 export { SCREEN } from './screen';
+export { SHADOW };
+export { FONT };
