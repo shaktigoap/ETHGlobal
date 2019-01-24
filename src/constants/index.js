a002ec542a1f8f7ed4d4a05a6fabfb40e5ef0151
ajayns <ajay.ns@outlook.com>
2019-01-24 09:03:04 +0530

    :sparkles: Init typography

--- src/constants/index.js
+++ src/constants/index.js
@@ -8,7 +8,16 @@ const FONT = {
   secondary: `'Muli', sans-serif`,
 };
 
+const FONT_SIZE = {
+  small: 'font-size: 18px, line-height: 22px',
+  regular: 'font-size: 20px; line-height: 32px',
+  large: 'font-size: 24px; line-height: 35px',
+  xlarge: 'font-size: 44px; line-height: 64px',
+  xxlarge: 'font-size: 71px; line-height: 85px',
+};
+
 export { COLOR } from './color';
 export { SCREEN } from './screen';
 export { SHADOW };
 export { FONT };
+export { FONT_SIZE };
