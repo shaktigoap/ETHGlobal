82fdeca9a3421ba5266f669e80012b4800e0b05d
ajayns <ajay.ns@outlook.com>
2019-01-24 12:04:58 +0530

    :recycle: Update typography

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,12 +4,14 @@ const SHADOW = {
 };
 
 const FONT = {
-  primary: `'Now', san-serif`,
+  primary: `'Now', Arial, san-serif`,
   secondary: `'Muli', sans-serif`,
 };
 
 const FONT_SIZE = {
-  small: 'font-size: 18px, line-height: 22px',
+  xxsmall: 'font-size: 14px; line-height: 18px',
+  xsmall: 'font-size: 16px; line-height: 30px',
+  small: 'font-size: 18px; line-height: 22px',
   regular: 'font-size: 20px; line-height: 32px',
   large: 'font-size: 24px; line-height: 35px',
   xlarge: 'font-size: 44px; line-height: 64px',
