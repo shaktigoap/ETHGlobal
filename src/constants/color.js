129abed1dcb539e7c69a7fa1f9af260392bfca0a
ajayns <ajay.ns@outlook.com>
2019-01-24 08:35:51 +0530

    :art: Add color palette

--- src/constants/color.js
+++ src/constants/color.js
@@ -1,3 +1,20 @@
 export const COLOR = {
-  white: '#fff',
+  blue: {
+    regular: 'rgba(86,102,246,1)',
+    light: 'rgba(86,102,246,0.1)',
+    xlight: 'rgba(86,102,246,0.05)',
+  },
+  blue_dark: {
+    regular: 'rgba(38,37,90,1)',
+    light: 'rgba(38,37,90,0.5)',
+    xlight: 'rgba(38,37,90,0.1)',
+  },
+  red: {
+    regular: 'rgba(255,104,149,1)',
+    light: 'rgba(255,104,149,0.1)',
+  },
+  white: {
+    regular: 'rgba(255,255,255,1)',
+    light: 'rgba(255,255,255,0.6)',
+  },
 };
