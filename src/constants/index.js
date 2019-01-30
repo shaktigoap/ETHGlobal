c342ac257581ea0a673d434688471b2a5306a76e
ajayns <ajay.ns@outlook.com>
2019-01-30 15:54:03 +0530

    :fire: Constants -> theme

--- src/constants/index.js
+++
@@ -1,9 +0,0 @@
-const SHADOW = {
-  light: '0 10px 30px 0 rgba(66,75,158,0.18)',
-  regular: '0px 4px 15px 0px rgba(1,9,62,0.2)',
-};
-
-export { COLOR } from './color';
-export { SCREEN } from './screen';
-export { SHADOW };
-export { FONT, FONT_SIZE } from './typography';
---
+++ src/theme.js
@@ -0,0 +1,45 @@
+export const theme = {
+  font: {
+    primary: `'Now', Arial, san-serif`,
+    secondary: `'Muli', sans-serif`,
+  },
+  font_size: {
+    xxsmall: 'font-size: 14px; line-height: 18px',
+    xsmall: 'font-size: 16px; line-height: 30px',
+    small: 'font-size: 18px; line-height: 22px',
+    regular: 'font-size: 20px; line-height: 32px',
+    large: 'font-size: 24px; line-height: 35px',
+    xlarge: 'font-size: 44px; line-height: 64px',
+    xxlarge: 'font-size: 71px; line-height: 85px',
+  },
+  color: {
+    blue: {
+      regular: 'rgba(86,102,246,1)',
+      light: 'rgba(86,102,246,0.1)',
+      xlight: 'rgba(86,102,246,0.05)',
+    },
+    blue_dark: {
+      regular: 'rgba(38,37,90,1)',
+      light: 'rgba(38,37,90,0.5)',
+      xlight: 'rgba(38,37,90,0.1)',
+    },
+    red: {
+      regular: 'rgba(255,104,149,1)',
+      light: 'rgba(255,104,149,0.1)',
+    },
+    white: {
+      regular: 'rgba(255,255,255,1)',
+      light: 'rgba(255,255,255,0.6)',
+    },
+  },
+  screen: {
+    xs: '576px',
+    sm: '768px',
+    md: '992px',
+    lg: '1200px',
+  },
+  shadow: {
+    light: '0 10px 30px 0 rgba(66,75,158,0.18)',
+    regular: '0px 4px 15px 0px rgba(1,9,62,0.2)',
+  },
+};
