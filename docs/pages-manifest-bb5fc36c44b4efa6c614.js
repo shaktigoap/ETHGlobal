bba939e153ded469648af3b6d2f1cd966f93498f
Shakti Goap <shaktigoap@users.noreply.github.com>
2019-02-18 01:14:59 +0530

    updated docs

---
+++ docs/pages-manifest-bb5fc36c44b4efa6c614.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{138:function(s){s.exports={pages:[{componentChunkName:"component---src-pages-index-js",jsonName:"index",path:"/"}],dataPaths:{index:"173/path---index-6a9-NZuapzHg3X9TaN1iIixfv1W23E","sq--src-components-sections-events-js":2736460011,"sq--src-components-sections-header-js":2403480109,"sq--src-components-sections-team-js":2320052113}}}}]);
+//# sourceMappingURL=pages-manifest-bb5fc36c44b4efa6c614.js.map
